823f91234bceeea89083fac79184966de37180d2
Matt Hickey <mjh060@mun.ca>
2019-11-06 16:25:55 -0330

    test stuff

---
+++ testsrc/tests/MockDestination.java
@@ -0,0 +1,25 @@
+package tests;
+
+import paragraphing.DestinationI;
+
+public class MockDestination implements DestinationI {
+	
+	private String[] lines = {};
+	
+	public MockDestination() {
+	
+	}
+	
+
+	@Override
+	public void addLines(String[] lines) {
+		// TODO Auto-generated method stub
+		this.lines = lines;
+	}
+	
+	public String[] result() {
+		return this.lines;
+	}
+	
+	
+}
