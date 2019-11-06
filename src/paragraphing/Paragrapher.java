5fcb2a9a35d0fdf7ff205c45ee278b0d160492af
Matt Hickey <mjh060@mun.ca>
2019-11-06 14:45:39 -0330

    stub

---
+++ src/paragraphing/Paragrapher.java
@@ -0,0 +1,33 @@
+package paragraphing;
+
+public class Paragrapher implements ParagrapherI {
+
+	public Paragrapher(DestinationI inDest) {
+		
+	}
+	
+	@Override
+	public void setWidth(int width) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addWord(String[] parts) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addWord(String word) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ship() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
